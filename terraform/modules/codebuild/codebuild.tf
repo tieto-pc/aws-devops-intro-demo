f0ec4799a06c267d0577afabb3944ded0272c22a
Kari Marttila <kari.marttila@tieto.com>
2019-04-01 19:51:12 +0300

    CodeBuild IAM role ready

--- terraform/modules/codebuild/codebuild.tf
+++ terraform/modules/codebuild/codebuild.tf
@@ -6,8 +6,38 @@ locals {
 resource "aws_s3_bucket" "codebuild_s3_cache_bucket" {
   bucket = "${local.my_name}-cache-bucket"
   acl    = "private"
+
   tags {
     Name = "${local.my_name}-cache-bucket"
+    Environment = "${local.my_env}"
+    Prefix = "${var.prefix}"
+    Env = "${var.env}"
+    Region = "${var.region}"
+    Terraform = "true"
+  }
+}
+
+
+resource "aws_iam_role" "codebuild_iam_role" {
+  name = "${local.my_name}-iam-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+
+  tags {
+    Name = "${local.my_name}-iam-role"
     Environment = "${local.my_env}"
     Prefix = "${var.prefix}"
     Env = "${var.env}"
