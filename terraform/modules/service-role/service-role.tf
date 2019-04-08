7b6db7b5f5ea33f3d31cee81bef265ded91dab9c
Kari Marttila <kari.marttila@tieto.com>
2019-04-08 15:53:43 +0300

    Added missing right to download code from codecommit - now codebuild ok

--- terraform/modules/service-role/service-role.tf
+++ terraform/modules/service-role/service-role.tf
@@ -4,7 +4,6 @@ locals {
 }
 
 
-
 # Removed ec2 rights, let's see if they are actually needed for the service.
 resource "aws_iam_role_policy" "codebuild_iam_role_policy" {
   role = "${aws_iam_role.codebuild_iam_role.name}"
@@ -34,6 +33,15 @@ resource "aws_iam_role_policy" "codebuild_iam_role_policy" {
         "${var.s3_cache_bucket_arn}/*",
         "${var.s3_log_bucket_arn}",
         "${var.s3_log_bucket_arn}/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codecommit:GitPull"
+      ],
+      "Resource": [
+        "arn:aws:codecommit:eu-west-1:943670737262:devopsintro-dev-codecommit-repo"
       ]
     }
   ]
